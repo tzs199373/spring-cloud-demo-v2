dd7018fb6f13d7a07c210c6db9b6aa3f4bdd15f1
����ɽ <544610616@qq.com>
2020-03-23 16:17:45 +0800

    1

--- consumer-3333/src/main/java/com/example/consumer/Consumer3333Application.java
+++ consumer-3333/src/main/java/com/example/consumer/Consumer3333Application.java
@@ -1,8 +1,10 @@
 package com.example.consumer;
 
+import com.example.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableFeignClients(basePackages= {"com.example.common"})
 @ComponentScan({"com.example.common","com.example.consumer"})
+@RibbonClient(name="provider",configuration=MySelfRule.class)
 public class Consumer3333Application {
 
 	public static void main(String[] args) {
