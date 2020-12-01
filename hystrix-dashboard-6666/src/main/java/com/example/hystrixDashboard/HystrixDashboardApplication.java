009c5f2287a28de16e998ca0043fa30313fc97b0
����ɽ <544610616@qq.com>
2020-12-01 16:03:41 +0800

    1

---
+++ hystrix-dashboard-6666/src/main/java/com/example/hystrixDashboard/HystrixDashboardApplication.java
@@ -0,0 +1,26 @@
+package com.example.hystrixDashboard;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean hystrixMetricsStreamServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+		registration.addUrlMappings("/hystrix.stream");
+		return registration;
+	}
+}
---
+++ hystrix-dashboard-6666/src/test/java/com/example/hystrixDashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.hystrixDashboard;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HystrixDashboardApplicationTests {
+
+
+}
